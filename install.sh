ae5d1e62225f1a2c288d6f055bef942f2e6405d4
Ronald van Kouwen <ronald@keuken.bttf.lan>
2021-10-24 20:02:07 +0200

    now it actualy works

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
-
 cd $HOME
 git clone https://github.com/ronaldvk90/shell
 git clone https://github.com/romkatv/powerlevel10k.git
 mkdir $HOME/.zsh
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.zsh
-git clone https://github.com/zsh-users/zsh-completions.git $HOME/.zsh
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.zsh
+
+cd $HOME/.zsh
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/zsh-users/zsh-completions.git
+git clone https://github.com/zsh-users/zsh-autosuggestions.git
+cd $HOME
+
 cp $HOME/shell/.p10k.zsh $HOME
 cp $HOME/shell/.zshrc $HOME
 
