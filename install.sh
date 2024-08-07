86cc50d3df06aad669b2a894134a95c516c2ade1
Ronald van Kouwen <ronald@jennifer.bttf.lan>
2024-08-07 22:18:48 +0200

    Made it NixOS ready

--- install.sh
+++ install.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-cd $HOME
-curl -s https://ohmyposh.dev/install.sh | sudo bash -s -- -d /usr/local/bin
 oh-my-posh font install Hack
 mkdir $HOME/.zsh
-mkdir $HOME/oh-my-posh
+mkdir $HOME/.oh-my-posh
 
-git clone https://github.com/zsh-users/zsh-autosuggestions.git .zsh/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git .zsh/zsh-syntax-highlightings
-git clone https://github.com/zsh-users/zsh-completions.git .zsh/zsh-completions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.zsh/zsh-syntax-highlightings
+git clone https://github.com/zsh-users/zsh-completions.git $HOME/.zsh/zsh-completions
 
 cp $HOME/shell/.zshrc $HOME
-cp $HOME/shell/easy-term.omp.json $HOME/oh-my-posh
+cp $HOME/shell/easy-term.omp.json $HOME/.oh-my-posh
 cd ~
